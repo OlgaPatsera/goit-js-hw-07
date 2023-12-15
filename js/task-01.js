58b1210fdb13051c65532f546dc835b3c5d0563b
OlgaPatsera <110819493+OlgaPatsera@users.noreply.github.com>
2023-12-15 19:44:58 +0200

    commit-task1

--- js/task-01.js
+++ js/task-01.js
@@ -2,19 +2,14 @@
 
 ///Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 //Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
-//На що буде звертати увагу ментор при перевірці:
-//Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
-//Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
-//У консолі має бути виведено наступне повідомлення:
-"use strict";
 
-const totalCategories = document.querySelectorAll(".item");
-console.log(`В списке ${totalCategories.length} категории.`);
+const items = document.querySelectorAll('.item');
 
-const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Category: ${categories.children[0].textContent}
-Количество элементов: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray);
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach(element => {
+  const elementTitle = element.querySelector('h2').textContent;
+  console.log(`Category: ${elementTitle}`);
+  const elementsLength = element.querySelectorAll('li').length;
+  console.log(`Elements: ${elementsLength}`);
+});
