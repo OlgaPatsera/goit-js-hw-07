655b5990d9905fe122f901f16f3d227645d9c59c
OlgaPatsera <110819493+OlgaPatsera@users.noreply.github.com>
2023-12-17 17:59:29 +0200

    commit-2-6

--- js/task-02.js
+++ js/task-02.js
@@ -12,18 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
   {
-    url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Elephant Beside on Baby Elephant',
   },
   {
-    url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Blue Geeen and Orange Parrot',
   },
   {
-    url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Zebras on Zebra',
   },
 ];
@@ -40,7 +37,9 @@ const images = [
 // Додай мінімальне оформлення галереї флексбоксами через CSS класи.
 
 const galleryItem = document.querySelector('.gallery');
-console.log(galleryItem);
-const markup=images.map((image)=>`<li class="list-item"><img src="${image.url}" alt="${image.alt}" width=360 height=300 /></li>`);
-galleryItem.insertAdjacentHTML("afterbegin", markup);
-galleryItem.setAttribute("style", "list-style:none; display: flex; flex-wrap: wrap;");
+const markup = images.map(
+  image =>
+    `<li class="list-item"><img src="${image.url}" alt="${image.alt}" width=360 height=300 /></li>`
+);
+galleryItem.insertAdjacentHTML('afterbegin', markup);
+galleryItem.setAttribute('style', 'list-style:none; display: flex; flex-wrap: wrap;');
